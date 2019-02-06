93f2e7725675ef912e70914a44abd6ea0def8f55
Julius Kördel <kordel.julius@gmail.com>
2019-02-06 09:59:00 +0100

    fixa func

--- TNM097.m
+++ TNM097.m
@@ -1,3 +1,5 @@
-%% TNM097
+function [ image ] = TNM097( im )
+% TNM097
 % insert code that calls functions here
 
+image= im;
--- main.m
+++ main.m
@@ -7,11 +7,11 @@ close all;
 clc;
 
 % Load testimage
-path = fullfile('Images', 'imagename'); % 3,5
+path = fullfile('Images', 'test1.png'); % 3,5
 testImage = imread(path);
 testImage = im2double(testImage);
 % Call function
-output = TNM097
+output = TNM097(testImage)
 
 %% Show image
 figure();
