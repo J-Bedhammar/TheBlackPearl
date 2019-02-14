2c19ae9a79157981f6a07737c8ed5d33b2830b9c
Julius Kördel <kordel.julius@gmail.com>
2019-02-14 11:30:38 +0100

    commentys

--- pearlColors.m
+++ pearlColors.m
@@ -4,26 +4,31 @@ if pearlSize < 1
     pearlSize = 1;
 end
 
+% decide steplength in each colorchannel
 colorStepR = 1 / RGBRange(1);
 colorStepG = 1 / RGBRange(2);
 colorStepB = 1 / RGBRange(3);
+% set up return vars
 pearlPlateSize = pearlSize *RGBRange(1)*RGBRange(2)*RGBRange(3);
 colorArray = zeros(pearlSize,pearlPlateSize,3);
 pearlSingleArray = zeros(1,(RGBRange(1)*RGBRange(2)*RGBRange(3)),3);
+% set up steppings work 
 counter = 1;
 lowerLimit = 1;
 upperLimit = pearlSize;
-
-for r = 1 : RGBRange(1)%:stepSizeL:rangeL%1:setpSizeL:rangeL
+% loop through each color chanel to create pearlcolors
+for r = 1 : RGBRange(1)
     for g = 1 : RGBRange(2)
         for b =   1 : RGBRange(3)
+            %array for comparing
             pearlSingleArray(1,counter,1) = colorStepR*r;
             pearlSingleArray(1,counter,2) = colorStepG*g;
             pearlSingleArray(1,counter,3) = colorStepB*b;
-
+            % array for view
             colorArray(:,lowerLimit:upperLimit,1) = colorStepR*r;
             colorArray(:,lowerLimit:upperLimit,2) = colorStepG*g;
             colorArray(:,lowerLimit:upperLimit,3) = colorStepB*b;
+            % range and index fix for next iteration
             lowerLimit = pearlSize*counter ;
             counter = counter+1;
             upperLimit = pearlSize*counter;
