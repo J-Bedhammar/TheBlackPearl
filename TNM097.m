d348a8c071ca7b76e7722fc20d67aac0c04e9f83
Julius Kördel <kordel.julius@gmail.com>
2019-02-14 10:50:04 +0100

    color array

--- TNM097.m
+++ TNM097.m
@@ -2,7 +2,9 @@ function [ image ] = TNM097( im ,numberOfColors,numberOfL)
 % TNM097
 % insert code that calls functions here
 ppi = 200;
-pearls = pearlColors(numberOfColors,numberOfL);
+pearlSize = 40;
+RGBRange = [4,5,5];
+[pearlColors,pearlSingleArray] = pearlColors(pearlSize,RGBRange);
 
 
 
