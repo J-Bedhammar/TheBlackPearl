cb0d1b83407816cec9a0c38ba681c0dd141102d4
Julius Kördel <kordel.julius@gmail.com>
2019-02-15 14:21:57 +0100

    bugfix

--- assemble.m
+++ assemble.m
@@ -17,12 +17,14 @@ for r = 1:RowDist:x
            pearlifiedIm(lastRow:RowDist*counterx,lastCol:ColDist*countery,2) = pearlSingleArray(1,indexMatrix(counterx,countery),2);
            pearlifiedIm(lastRow:RowDist*counterx,lastCol:ColDist*countery,3) = pearlSingleArray(1,indexMatrix(counterx,countery),3);
            % indexing and access stuff
-           countery = countery+1;
            lastCol = ColDist*countery;
+           countery = countery+1;
+      
     end
     % indexing and access stuff
     % and reset y for each row
     countery = 1; 
+    lastCol = 1;
     counterx = counterx+1;
     lastRow = RowDist*counterx;
 end
--- meanColorInGrid.m
+++ meanColorInGrid.m
@@ -15,18 +15,34 @@ for r = 1:RowDist:x
     for c = 1 : ColDist : y
         % goes through each grid area in the og im and creates a mean value
         % for that gridim. 
-        meanGrid(counterx,countery,1)=mean(mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,1)));
+        r1 = mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,1));
+        if(isempty(r1))
+        a=RowDist*counterx
+        lastRow
+        
+        c
+        lastCol
+        b=ColDist*countery
+        end
+        
+        g1 = mean(im(lastRow:r,lastCol:c,2));
+        b1 = mean(im(lastRow:r,lastCol:c,3));
+        
+        meanGrid(counterx,countery,1)=mean(mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,1)));      
         meanGrid(counterx,countery,2)=mean(mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,2)));
         meanGrid(counterx,countery,3)=mean(mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,3)));
         % indexing and access stuff
+        
+        lastCol = (ColDist*countery);
         countery = countery+1;
-        lastCol = ColDist*countery;
     end
     % indexing and access stuff
     % and reset y for each row
-    countery = 1; 
+    countery = 1;
+    lastCol = 1;
+    lastRow = (RowDist*counterx) +1;
     counterx = counterx+1;
-    lastRow = RowDist*counterx;
+    
 end
 
 
