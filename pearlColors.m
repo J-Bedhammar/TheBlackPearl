8f0e1f0ab3f1955eac9322344e7a634c6ae43af6
Jennifer Bedhammar <jennifer95@hotmail.com>
2019-02-18 10:13:42 +0100

    New pearlPlate appearance

--- pearlColors.m
+++ pearlColors.m
@@ -9,14 +9,17 @@ colorStepR = 1 / RGBRange(1);
 colorStepG = 1 / RGBRange(2);
 colorStepB = 1 / RGBRange(3);
 % set up return vars
-pearlPlateSize = pearlSize *RGBRange(1)*RGBRange(2)*RGBRange(3);
+pearlPlateSize = pearlSize*RGBRange(2)*RGBRange(3); %pearlSize*RGBRange(1)*RGBRange(2)*RGBRange(3);
 colorArray = zeros(pearlSize,pearlPlateSize,3);
 pearlSingleArray = zeros(1,(RGBRange(1)*RGBRange(2)*RGBRange(3)),3);
 % set up steppings work 
 counter = 1;
+counter2 = 1;
 lowerLimit = 1;
 upperLimit = pearlSize;
-% loop through each color chanel to create pearlcolors
+firstRow = 1;
+lastRow = pearlSize;
+% loop through each color channel to create pearlcolors
 for r = 1 : RGBRange(1)
     for g = 1 : RGBRange(2)
         for b =   1 : RGBRange(3)
@@ -25,15 +28,23 @@ for r = 1 : RGBRange(1)
             pearlSingleArray(1,counter,2) = colorStepG*g;
             pearlSingleArray(1,counter,3) = colorStepB*b;
             % array for view
-            colorArray(:,lowerLimit:upperLimit,1) = colorStepR*r;
-            colorArray(:,lowerLimit:upperLimit,2) = colorStepG*g;
-            colorArray(:,lowerLimit:upperLimit,3) = colorStepB*b;
+            colorArray(firstRow:lastRow,lowerLimit:upperLimit,1) = colorStepR*r;
+            colorArray(firstRow:lastRow,lowerLimit:upperLimit,2) = colorStepG*g;
+            colorArray(firstRow:lastRow,lowerLimit:upperLimit,3) = colorStepB*b;
             % range and index fix for next iteration
-            lowerLimit = pearlSize*counter ;
+            lowerLimit = pearlSize*counter;
             counter = counter+1;
             upperLimit = pearlSize*counter;
         end
     end
+    % Reset columns
+    lowerLimit = 1;
+    upperLimit = pearlSize;
+    counter = 1;
+    % Range and index for next row
+    firstRow = pearlSize*counter2 ;
+    counter2 = counter2+1;
+    lastRow = pearlSize*counter2;
 end
 
 pearlPlate = colorArray;
