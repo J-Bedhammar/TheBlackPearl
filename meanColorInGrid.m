52311f5c03a3f9fbf939c2b81ba10adb7f608993
Julius Kördel <kordel.julius@gmail.com>
2019-02-18 09:19:29 +0100

    fixed for uneven image

--- meanColorInGrid.m
+++ meanColorInGrid.m
@@ -4,7 +4,7 @@ function [meanGrid] = meanColorInGrid(im,ColDist,RowDist)
 % size for length in x and y axis
 [x,y]=size(im(:,:,1)); 
 % grid that contains all means
-meanGrid = zeros(x/RowDist,y/ColDist,3);
+meanGrid = zeros(floor(x/RowDist),floor(y/ColDist),3);
 % for indexing and acces in im
 counterx = 1;
 countery = 1; 
@@ -15,26 +15,32 @@ for r = 1:RowDist:x
     for c = 1 : ColDist : y
         % goes through each grid area in the og im and creates a mean value
         % for that gridim. 
-        r1 = mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,1));
-        if(isempty(r1))
-        a=RowDist*counterx
-        lastRow
-        
-        c
-        lastCol
-        b=ColDist*countery
+%         r1 = mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,1));
+%         if(isempty(r1))
+%         a=RowDist*counterx
+%         lastRow
+%         
+%         c
+%         lastCol
+%         b=ColDist*countery
+%         end
+%         
+%         g1 = mean(im(lastRow:r,lastCol:c,2));
+%         b1 = mean(im(lastRow:r,lastCol:c,3));
+%         
+        if(RowDist*counterx > x || ColDist*countery > y) 
+            continue;
         end
-        
-        g1 = mean(im(lastRow:r,lastCol:c,2));
-        b1 = mean(im(lastRow:r,lastCol:c,3));
-        
-        meanGrid(counterx,countery,1)=mean(mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,1)));      
+       
+        meanGrid(counterx,countery,1)=mean(mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,1)));
         meanGrid(counterx,countery,2)=mean(mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,2)));
         meanGrid(counterx,countery,3)=mean(mean(im(lastRow:RowDist*counterx,lastCol:ColDist*countery,3)));
         % indexing and access stuff
         
-        lastCol = (ColDist*countery);
+        
+        lastCol = (ColDist*countery); 
         countery = countery+1;
+        
     end
     % indexing and access stuff
     % and reset y for each row
@@ -42,6 +48,7 @@ for r = 1:RowDist:x
     lastCol = 1;
     lastRow = (RowDist*counterx) +1;
     counterx = counterx+1;
+    
     
 end
 
