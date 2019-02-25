0926c534303c6c06295ee6c6c7fcfad19bfb8774
Jennifer Bedhammar <jennifer95@hotmail.com>
2019-02-25 12:12:12 +0100

    Removed all .asv from git

--- TNM097.m
+++ TNM097.m
@@ -1,9 +1,9 @@
-function [ image ] = TNM097( im ,numberOfColors,numberOfL)
+function [ image ] = TNM097(im ,numberOfColors,numberOfL)
 % TNM097
 % insert code that calls functions here
 
 % Add grid to image
-PearlsPerRow = 60;
+PearlsPerRow = 20;
 [imGrid, ColDist, RowDist] = addGrid(im, PearlsPerRow);
 
 ppi = 200;
@@ -12,33 +12,20 @@ pearlSize = 40;
 % Pearl colors:
 % 1 = 100, 2 = 80, 3 = 64, 4 = 48, 5 = 27, 6 = 18, 7 = 12, 8 = 8, 9 = 4
 RGBRange = colorSteps(1);
-RGBRange2 = colorSteps(4);
 [pearlPlate,pearlSingleArray] = pearlColors(pearlSize,RGBRange);
-[pearlPlate2,pearlSingleArray2] = pearlColors(pearlSize,RGBRange);
-% lessPerls = lessNumberOfPearls(pearlSingleArray,50);
-
-%imshow(pearlPlate);
 
 % Mean colors of grid squares
 [meanGrid] = meanColorInGrid(im,ColDist,RowDist);
 
 indexPearlGrid = indexColorMatch(pearlSingleArray, meanGrid);
-% indexPearlGrid2 = indexColorMatch(pearlSingleArray2, meanGrid);
-% indexPearlGrid3 = indexColorMatch(lessPerls, meanGrid);
 
-
-figure
 [pearlifiedIm] = assemble(indexPearlGrid,ColDist,RowDist,size(im),pearlSingleArray);
-% figure
-% [pearlifiedIm2] = assemble(indexPearlGrid2,ColDist,RowDist,size(im),pearlSingleArray2);
-% figure
-% [pearlifiedIm2] = assemble(indexPearlGrid3,ColDist,RowDist,size(im),lessPerls);
-
 
 [X_no_dither,map] = rgb2ind(im,8,'nodither');
+%imshow(X_no_dither,map)
 
-pearls = drawCircles(im, ColDist, RowDist, indexPearlGrid, pearlSingleArray);
+%imshow(pearlPlate)
 
-image = pearls;
+image = pearlifiedIm;
 
 end
