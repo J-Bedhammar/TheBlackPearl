313aee5bad9356465418785c4f1333fb11d6eae1
Sparvolle <Oliverj96@hotmail.com>
2019-02-27 15:26:29 +0100

    Update aggGrid, takes in pearlsize instead of pearls per row

--- addGrid.m
+++ addGrid.m
@@ -1,23 +1,18 @@
-function [PearlsPerCol, ColDist, RowDist] = addGrid(inimg,PearlsPerRow)
+function [PearlsPerRow, PearlsPerCol] = addGrid(inimg,PearlSize)
 % Author: Oliver Johansson
 % Adds a grid ontop of the image to visualize the pearls position
 
 % Calculates the dimensions of the image
-[height, width, dim] = size(inimg);
+[Height, Width, dim] = size(inimg);
+
+PearlsPerRow = floor(Width/PearlSize);
 
 % Calculates the ratio between the height and the width of the image
 ratio = height/width;
 
 % Calculates the number of pearls per column depedning on pearls per rows
 % and the ratio of the image.
-PearlsPerCol = PearlsPerRow*ratio;
-
-% Calculates the distance between the rows and cols
-ColDist = floor(width/PearlsPerRow);
-RowDist = floor(height/PearlsPerCol);
-
-% Floor PearlsPerCol
-PearlsPerCol = ceil(PearlsPerCol);
+PearlsPerCol = ceil(PearlsPerRow*ratio);
 
 end
 
